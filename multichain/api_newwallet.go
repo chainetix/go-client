1a1d216dc404a597e2c8b754e5587bc87c9ade0a
Alexander Breadman <alex@Alexanders-MacBook-Air.local>
2018-06-04 22:20:28 +0100

    fix new user collision

--- multichain/api_newwallet.go
+++ multichain/api_newwallet.go
@@ -3,9 +3,9 @@ package chainetix
 func (client *Client) NewWallet(label string, secret string, user string) (map[string]interface{}, error) {
 	dest := map[string]interface{}{}
 	src := map[string]interface{}{
+		"secret": secret,
 		"user": user,
 		"label": label,
-		"secret": secret,
 	}
 	_, err := client.Post(
 		"https://multichain.blokhub.io/api/wallet",
